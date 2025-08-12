b1b071ec7de9aaa786356ef34df548e51062b114
carltonjohnson1 <carltonj1@hotmail.com>
2025-08-12 10:11:19 +0100

    updated to make 30 sec timer work

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
+timeText = document.querySelector(".time b"),
 inputField = document.querySelector("input"),
 refreshBtn = document.querySelector(".refresh-word"),
 checkBtn = document.querySelector(".check-word");
 
-let correctWord; 
+let correctWord, timer; 
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if(maxTime > 0) {
+            maxTime--; // decrement maxTime by -1
+            return timeText.innerText = maxTime;
+        }
+        clearInterval(timer);
+        alert(`Time off! ${correctWord.toUpperCase()} is a correct word`);
+        initGame(); // calling initGame function, so the game restarts
+    }, 1000);
+}
 
 const initGame = () => {
+    initTimer(30); // calling iniTimer function with passing 30 as maxTime value
     let randomObj = words[Math.floor(Math.random() * words.length)]; // getting random object from words
     let wordArray = randomObj.word.split(""); // splitting each letter of random word
     for (let i = wordArray.length - 1; i > 0; i--){
@@ -17,14 +32,23 @@ const initGame = () => {
     wordText.innerText = wordArray.join(""); // passing shuffled word as word text
     hintText.innerText = randomObj.hint; //passing random object hint as hint text
     correctWord = randomObj.word.toLowerCase(); // passing random word to correctWord
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length); // setting input maxlength attr value to word length
     console.log(randomObj);
 
 }
 initGame();
 
 const checkWord = () => {
-    let userWord = inputField.value.toLowerCase();
+    let userWord = inputField.value.toLowerCase(); // getting user value
+    if(!userWord) return alert("Please enter a word to check"); // if user didn't etner anything
+
+    // if users word does not match the correct word
     if(userWord !== correctWord) return alert(`Oops! ${userWord} is not a correct word`);
+
+    // if users word matches the correct word
+    alert(`Congrats! ${userWord.toUpperCase()} is a correct word`);
+    initGame();
 }
 
 refreshBtn.addEventListener("click", initGame);
